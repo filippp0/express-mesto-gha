c5ff1568c2cca22d3c855bfa34bf32394f1d4ba6
Filipp Kolesnikov <filippp0@yandex.ru>
2023-07-25 20:01:10 +0300

    correct 6

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.patch('*', (req, res) => {
+  res.status(404).send({ message: 'страница не найдена.' });
+});
+
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'страница не найдена.' });
 });
