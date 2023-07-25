8771f0ffe2dd98b063b148a59a368dfa8d8fa256
Filipp Kolesnikov <filippp0@yandex.ru>
2023-07-25 19:17:22 +0300

    корретировка 2

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,13 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUserById = (req, res) => {
   if (req.params.userId.length === 24) {
     User.findById(req.params.userId)
-      .then((user) => res.send(user))
+      .then((user) => {
+        if (!user) {
+          res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+          return;
+        }
+        res.send(user);
+      })
       .catch(() => res.status(404).send({ message: 'Пользователь по указанному _id не найден.' }));
   } else {
     res.status(400).send({ message: 'Некорректный _id' });
