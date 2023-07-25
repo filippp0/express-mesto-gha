0b72d0066c16c95aa3bce3e0039d4940d6ff2d72
Filipp Kolesnikov <filippp0@yandex.ru>
2023-07-25 18:29:07 +0300

    корректировака ошибок 1

--- controllers/users.js
+++ controllers/users.js
@@ -7,12 +7,12 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  if (req.params.userId) {
+  if (req.params.userId.length === 24) {
     User.findById(req.params.userId)
       .then((user) => res.send(user))
       .catch(() => res.status(404).send({ message: 'Пользователь по указанному _id не найден.' }));
   } else {
-    res.status(500).send({ message: 'Ошибка по умолчанию.' });
+    res.status(400).send({ message: 'Некорректный _id' });
   }
 };
 
