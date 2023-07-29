cc1e24126c968efe9428509c574d3478e4974360
Filipp Kolesnikov <filippp0@yandex.ru>
2023-07-29 17:35:34 +0300

    test2

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-// app.use('*', (req, res) => {
-//   res.status(404).send({ message: 'страница не найдена.' });
-// });
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'страница не найдена.' });
+});
 
 app.listen(PORT);
