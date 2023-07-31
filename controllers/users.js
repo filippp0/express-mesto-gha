d45fa893434052af1211508419fce3436adfd190
Filipp Kolesnikov <filippp0@yandex.ru>
2023-07-31 15:36:16 +0300

    test error 1

--- controllers/users.js
+++ controllers/users.js
@@ -6,20 +6,37 @@ module.exports.getUsers = (req, res) => {
     .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
+// module.exports.getUserById = (req, res) => {
+//   if (req.params.userId.length === 24) {
+//     User.findById(req.params.userId)
+//       .then((user) => {
+//         if (!user) {
+//           res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+//           return;
+//         }
+//         res.send(user);
+//       })
+//     .catch(() => res.status(404).send({ message: 'Пользователь по указанному _id не найден.' }));
+//   } else {
+//     res.status(400).send({ message: 'Некорректный _id' });
+//   }
+// };
+
 module.exports.getUserById = (req, res) => {
-  if (req.params.userId.length === 24) {
-    User.findById(req.params.userId)
-      .then((user) => {
-        if (!user) {
-          res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
-          return;
-        }
-        res.send(user);
-      })
-      .catch(() => res.status(404).send({ message: 'Пользователь по указанному _id не найден.' }));
-  } else {
-    res.status(400).send({ message: 'Некорректный _id' });
-  }
+  User.findById(req.params.userId)
+    .orFail()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: `Некорректный _id: ${req.params.userId}` });
+      } else if (err.name === 'DocumentNotFoundError') {
+        res.status(404).send({ message: `Пользователь по указанному _id: ${req.params.userId} не найден.` });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 
 module.exports.addUser = (req, res) => {
