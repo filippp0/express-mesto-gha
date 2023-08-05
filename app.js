d5aeef4dcddcedbe1b5f920feb8c8f12fd698bca
Filipp Kolesnikov <filippp0@yandex.ru>
2023-08-05 11:16:46 +0300

    корректировка ошибок через new

--- app.js
+++ app.js
@@ -27,13 +27,14 @@ app.use('*', (req, res) => {
   res.status(404).send({ message: 'страница не найдена.' });
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500
       ? 'На сервере произошла ошибка'
       : message,
   });
+  next();
   // res.status(err.statusCode).send({ message: err.message });
 });
 
