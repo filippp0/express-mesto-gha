affadb1c2af3dab7bfa5402f67b1c412947b43b7
Filipp Kolesnikov <filippp0@yandex.ru>
2023-08-19 13:26:47 +0300

    dobavit log

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
 // console.log(require('http2').constants);
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
@@ -29,7 +30,11 @@ mongoose.connect(DB_URL, {
   useUnifiedTopology: true,
 });
 
+app.use(requestLogger);
+
 app.use('/', require('./routes/index'));
+
+app.use(errorLogger);
 
 app.use(errors());
 
